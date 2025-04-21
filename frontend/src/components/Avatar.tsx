2e2f7f1bb33ac868b5c4bfd68fb41b2362741ee2
AleshaFozu <lexalis12@mail.ru>
2025-04-21 22:03:55 +0700

    ss

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import config from '../config';
 import styles from './Avatar.module.css';
 
 interface AvatarProps {
@@ -9,11 +8,7 @@ interface AvatarProps {
 }
 
 export const Avatar: React.FC<AvatarProps> = ({ avatarUrl, className = '', alt = 'Avatar' }) => {
-  const fullAvatarUrl = avatarUrl 
-    ? avatarUrl.startsWith('http') 
-      ? avatarUrl 
-      : `${config.baseUrl}${avatarUrl}`
-    : `${config.baseUrl}/uploads/default-avatar.png`;
+  const fullAvatarUrl = avatarUrl || '/images/default-avatar.svg';
 
   return (
     <div className={`${styles.avatarContainer} ${className}`}>
